df2523ae3a28690ae78769c29499a7a0957e383f
zhucj <zhucj@servyou.com.cn>
2021-03-12 16:00:38 +0800

    org.springframework.aop.framework.JdkDynamicAopProxy.invoke

--- spring-aop/src/main/java/org/aopalliance/intercept/MethodInvocation.java
+++ spring-aop/src/main/java/org/aopalliance/intercept/MethodInvocation.java
@@ -4,13 +4,13 @@ import javax.annotation.Nonnull;
 import java.lang.reflect.Method;
 
 /**
- * Description of an invocation to a method, given to an interceptor
- * upon method-call.
+ * Description of an invocation to a method, given to an interceptor upon method-call.
  *
  * <p>A method invocation is a joinpoint and can be intercepted by a
  * method interceptor.
  *
  * @author Rod Johnson
+ * @see org.springframework.aop.framework.JdkDynamicAopProxy#invoke(java.lang.Object, java.lang.reflect.Method, java.lang.Object[])#invoke(java.lang.Object, java.lang.reflect.Method, java.lang.Object[])
  * @see MethodInterceptor
  */
 public interface MethodInvocation extends Invocation {
