b97fc7a0ad656b361f79cce47b41517e352119de
zhucj <zhucj@servyou.com.cn>
2021-03-14 12:49:19 +0800

    A_B站圣思园Spring精髓课程

---
+++ xxlgood/src/main/java/com/shengsiyuan/spring/lecture/A_B站圣思园Spring精髓课程.java
@@ -0,0 +1,15 @@
+package com.shengsiyuan.spring.lecture;
+
+/**
+ * A_B站圣思园Spring课程
+ *
+ * @author zhucj
+ * @since 20210325
+ */
+public class A_B站圣思园Spring精髓课程 {
+
+	/**
+	 * https://www.bilibili.com/video/BV1p54y1U7Th?p=36&spm_id_from=pageDriver
+	 * 共37节课，每节课30分钟左右
+	 */
+}
